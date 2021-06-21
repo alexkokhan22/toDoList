dc63916332ffa18bf1fc530210f89cceee2bc4c0
Alex Kokhan <kokhan.a.a@yandex.by>
2021-06-21 18:42:52 +0300

    Updates

---
+++ precache-manifest.a731bdddda3c98e060ebb3959a71ca64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a7c7347361264bbb1726fc3caa5c80",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "9e5f776a63eb3e70b859",
+    "url": "/toDoList/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "d3af9dca06810be76bfb",
+    "url": "/toDoList/static/js/2.fc74ac89.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/toDoList/static/js/2.fc74ac89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5f776a63eb3e70b859",
+    "url": "/toDoList/static/js/main.29cd9726.chunk.js"
+  },
+  {
+    "revision": "51821ba2afdc6800bea7",
+    "url": "/toDoList/static/js/runtime-main.88fb5b6c.js"
+  }
+]);
